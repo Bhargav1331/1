ae473e7452490accc655edf259c5010cf8f6ea32
ashish201 <katariyaashish203@gmail.com>
2023-02-08 13:46:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+server.listen(port, () => {
+    console.log('listening on *:3000');
+});
